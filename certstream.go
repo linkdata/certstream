2fec5424bede841d53333c6b0be58f11ff3cf411
Johan Lindh <johan@linkdata.se>
2024-10-16 13:48:14 +0200

    wip

--- certstream.go
+++ certstream.go
@@ -14,27 +14,27 @@ import (
 
 type CertStream struct {
 	OperatorFilter func(op *loglist3.Operator) bool // if nil, accepts all operators
-	StatusFilter   func(loglist3.LogStatus) bool    // if nil, only accepts loglist3.LogStatusUsable
+	StatusFilter   func(loglist3.LogStatus) bool    // DefaultStatusFilter only accepts loglist3.LogStatusUsable
 	BatchSize      int
 	Workers        int
 	proxy.ContextDialer
 }
 
-func defaultStatusFilter(status loglist3.LogStatus) bool {
+func DefaultStatusFilter(status loglist3.LogStatus) bool {
 	return status == loglist3.UsableLogStatus
 }
 
 // New returns a CertStream with reasonable defaults.
 func New() *CertStream {
 	return &CertStream{
-		StatusFilter:  defaultStatusFilter,
+		StatusFilter:  DefaultStatusFilter,
 		BatchSize:     256,
 		Workers:       2,
 		ContextDialer: &net.Dialer{},
 	}
 }
 
-// Init returns the LogStreams that pass the filter functions and that initialize successfully.
+// Start returns a channel to read results from.
 func (cs *CertStream) Start(ctx context.Context, logList *loglist3.LogList) (certCh <-chan *certificate.Batch, err error) {
 	batchCh := make(chan *certificate.Batch, cs.Workers)
 	certCh = batchCh
