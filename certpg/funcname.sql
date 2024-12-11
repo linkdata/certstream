b966bdcc4510b89c78ecc24f8801251267fb28fd
Johan Lindh <johan@linkdata.se>
2024-12-11 10:43:29 +0100

    wip

--- certpg/funcname.sql
+++ certpg/funcname.sql
@@ -2,8 +2,18 @@ CREATE OR REPLACE FUNCTION CERTDB_name(
   IN _dnsname TEXT
 )
 RETURNS TEXT LANGUAGE plpgsql IMMUTABLE AS $$
+DECLARE
+	_a TEXT[];
 BEGIN
-	RETURN array_to_string(array_remove(trim_array(string_to_array(lower(_dnsname),'.'),1),'www'),'.');
+	_dnsname := lower(_dnsname);
+	IF substring(_dnsname for 4) = 'www.' THEN
+		_dnsname := substring(_dnsname from 5);
+	END IF;
+	_a := string_to_array(_dnsname, '.');
+	IF array_length(_a, 1) > 0 THEN
+		_a := trim_array(_a, 1);
+	END IF;
+	RETURN array_to_string(_a, '.');
 END;
 $$
 ;
