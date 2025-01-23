e19fc61f93dbc771586baff9ea799919770772c7
Johan Lindh <johan@linkdata.se>
2025-01-23 08:45:28 +0100

    wip

--- assets/funcoperatorid.sql
+++ assets/funcoperatorid.sql
@@ -9,12 +9,13 @@ DECLARE
   _id INTEGER;
 BEGIN
   SELECT id FROM CERTDB_operator INTO _id WHERE name=s_name AND email=s_email;
-  IF NOT FOUND THEN
+  IF _id IS NULL THEN
     INSERT INTO CERTDB_operator (name, email) VALUES (s_name, s_email)
       ON CONFLICT DO NOTHING RETURNING id INTO _id;
-  END IF;
-  IF NOT FOUND THEN
-    SELECT id FROM CERTDB_operator INTO _id WHERE name=s_name AND email=s_email;
+    COMMIT;
+    IF _id IS NULL THEN
+      SELECT id FROM CERTDB_operator INTO _id WHERE name=s_name AND email=s_email;
+    END IF;
   END IF;
   RETURN _id;
 END;
--- assets/funcstreamid.sql
+++ assets/funcstreamid.sql
@@ -10,12 +10,13 @@ DECLARE
   _id INTEGER;
 BEGIN
   SELECT id FROM CERTDB_stream INTO _id WHERE url=s_url;
-  IF NOT FOUND THEN
+  IF _id IS NULL THEN
     INSERT INTO CERTDB_stream (url, operator, json) VALUES (s_url, s_operator, s_json)
       ON CONFLICT DO NOTHING RETURNING id INTO _id;
-  END IF;
-  IF NOT FOUND THEN
-    SELECT id FROM CERTDB_stream INTO _id WHERE url=s_url;
+    COMMIT;
+    IF _id IS NULL THEN
+      SELECT id FROM CERTDB_stream INTO _id WHERE url=s_url;
+    END IF;
   END IF;
   RETURN _id;
 END;
