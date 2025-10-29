e3e736c1d8eefa942c3e86baee68ae89d0762ef4
Johan Lindh <johan@linkdata.se>
2025-10-29 13:52:02 +0100

    wip

--- pgdb.go
+++ pgdb.go
@@ -91,7 +91,7 @@ func NewPgDB(ctx context.Context, cs *CertStream) (cdb *PgDB, err error) {
 						workerCount := 1 << workerBits
 						batchChans := make([]chan *LogEntry, workerCount)
 						for i := range batchChans {
-							batchChans[i] = make(chan *LogEntry, 4096)
+							batchChans[i] = make(chan *LogEntry, DbBatchSize*2)
 						}
 						cdb = &PgDB{
 							CertStream:            cs,
