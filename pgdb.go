3dae24c8b43078624d0111d02dc8108f7d0f3d4b
Johan Lindh <johan@linkdata.se>
2026-01-28 14:05:10 +0100

    gapCh 8

--- pgdb.go
+++ pgdb.go
@@ -562,7 +562,7 @@ func (cdb *PgDB) selectAllGaps(ctx context.Context, wg *sync.WaitGroup) {
 		logop.mu.Lock()
 		for _, ls := range logop.streams {
 			if ls.gapCh == nil {
-				ls.gapCh = make(chan gap, 1)
+				ls.gapCh = make(chan gap, 8)
 			}
 			streams[ls.Id] = ls
 		}
