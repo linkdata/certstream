d67a6aa67b2b497f901b08b544fbbe6741fc0ffd
Johan Lindh <johan@linkdata.se>
2026-02-03 09:19:39 +0100

    add cache dir

--- sunlight_client.go
+++ sunlight_client.go
@@ -4,6 +4,8 @@ import (
 	"crypto/x509"
 	"errors"
 	"net/http"
+	"os"
+	"path"
 
 	"filippo.io/sunlight"
 	"github.com/google/certificate-transparency-go/loglist3"
@@ -17,12 +19,18 @@ func newSunlightClient(log *loglist3.TiledLog, httpClient *http.Client, concurre
 	var pub any
 	if log != nil {
 		if pub, err = x509.ParsePKIXPublicKey(log.Key); err == nil {
+			cachedir := path.Join(os.TempDir(), "certstream")
+			_ = os.RemoveAll(cachedir)
+			if e := os.Mkdir(cachedir, 0755); e != nil {
+				cachedir = ""
+			}
 			cfg := &sunlight.ClientConfig{
 				MonitoringPrefix: log.MonitoringURL,
 				PublicKey:        pub,
 				HTTPClient:       httpClient,
 				UserAgent:        sunlightUserAgent,
 				ConcurrencyLimit: max(concurrency, 1),
+				Cache:            cachedir,
 			}
 			client, err = sunlight.NewClient(cfg)
 		}
