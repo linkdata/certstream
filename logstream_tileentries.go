6c8d0fd8a39967e73e17665654608720c819c6b7
Johan Lindh <johan@linkdata.se>
2026-02-05 09:07:12 +0100

    wip

--- logstream_tileentries.go
+++ logstream_tileentries.go
@@ -53,89 +53,44 @@ func (ls *LogStream) makeTileLogEntry(logindex int64, entry *sunlight.LogEntry, 
 }
 
 func (ls *LogStream) getTileEntries(ctx context.Context, start, end int64, historical bool, handleFn handleLogEntryFn, gapcounter *atomic.Int64) (next int64, wanted bool) {
+	client := ls.headTile
+	if historical && ls.tailTile != nil {
+		client = ls.tailTile
+	}
+
 	next = start
-	if start <= end {
-		client := ls.headTile
-		if historical && ls.tailTile != nil {
-			client = ls.tailTile
-		}
-		if client != nil {
+	for next <= end {
+		entryErr := ls.backoff.Retry(ctx, func() (err error) {
 			var checkpoint sunlight.Checkpoint
-			var chkErr error
-			chkErr = ls.backoff.Retry(ctx, func() error {
-				ls.adjustTailLimiter(historical)
-				var err error
-				checkpoint, _, err = client.Checkpoint(ctx)
-				if err == nil {
-					return nil
-				}
-				if ls.handleStreamError(err, "Checkpoint") {
-					return err
-				}
-				return wrapLogStreamRetryable(err)
-			})
-			if chkErr == nil {
-				if checkpoint.N > 0 {
-					maxIndex := checkpoint.N - 1
-					if end > maxIndex {
-						end = maxIndex
+			if checkpoint, _, err = client.Checkpoint(ctx); err == nil {
+				for logindex, entry := range client.Entries(ctx, checkpoint.Tree, next) {
+					le := ls.makeTileLogEntry(logindex, entry, historical)
+					ls.seeIndex(logindex)
+					if handleFn(ctx, time.Now(), le) {
+						wanted = true
 					}
-					for start <= end {
-						if ctx.Err() == nil {
-							lastIndex := int64(-1)
-							entryErr := ls.backoff.Retry(ctx, func() error {
-								lastIndex = -1
-								ls.adjustTailLimiter(historical)
-								now := time.Now()
-								for i, entry := range client.Entries(ctx, checkpoint.Tree, start) {
-									if i > end {
-										break
-									}
-									le := ls.makeTileLogEntry(i, entry, historical)
-									ls.seeIndex(i)
-									if handleFn(ctx, now, le) {
-										wanted = true
-									}
-									lastIndex = i
-									next = i + 1
-									if gapcounter != nil {
-										gapcounter.Add(-1)
-									}
-								}
-								err := client.Err()
-								if err == nil {
-									return nil
-								}
-								if ls.handleStreamError(err, "Entries") {
-									return err
-								}
-								return wrapLogStreamRetryable(err)
-							})
-							if entryErr == nil {
-								if lastIndex >= start {
-									start = lastIndex + 1
-								} else {
-									start = end + 1
-								}
-							} else if gapcounter != nil && ctx.Err() == nil {
-								if lastIndex >= start {
-									start = lastIndex + 1
-								}
-								_ = ls.LogError(entryErr, "gap not fillable", "url", ls.URL(), "start", start, "end", end)
-								gapcounter.Add(start - (end + 1))
-								start = end + 1
-							} else {
-								start = end + 1
-							}
-						} else {
-							start = end + 1
-						}
+					next = logindex + 1
+					if gapcounter != nil {
+						gapcounter.Add(-1)
 					}
+					if next >= end {
+						break
+					}
+					ls.adjustTailLimiter(historical)
 				}
-			} else if gapcounter != nil && ctx.Err() == nil {
-				_ = ls.LogError(chkErr, "gap not fillable", "url", ls.URL(), "start", start, "end", end)
-				gapcounter.Add(start - (end + 1))
+				err = client.Err()
 			}
+			if !ls.handleStreamError(err, "getTileEntries") {
+				err = wrapLogStreamRetryable(err)
+			}
+			return
+		})
+		if entryErr != nil {
+			if gapcounter != nil && ctx.Err() == nil {
+				_ = ls.LogError(entryErr, "gap not fillable", "url", ls.URL(), "start", next, "end", end)
+				gapcounter.Add(next - (end + 1))
+			}
+			break
 		}
 	}
 	return
